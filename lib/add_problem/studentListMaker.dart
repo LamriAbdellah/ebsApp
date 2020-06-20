8b0b180e10d7cdd8e506c3f26dc605cc07004ed8
MI <a.lamri@esi-sba.dz>
2020-06-20 18:14:17 +0100

    worked2

--- lib/add_problem/studentListMaker.dart
+++ lib/add_problem/studentListMaker.dart
@@ -28,7 +28,7 @@ final user =Provider.of<User>(context);
                 itemCount: students.length,
 itemBuilder: (context,index)
 {
-  
+
             if ((students[index].getProp(widget.module)>=user.getProp(widget.module))&&(students[index].name!=user.pseudo)) {
 
             return Card(
