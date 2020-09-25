73ffa6ef41b37f9fa640f002932c0c41345c9bdf
MI <a.lamri@esi-sba.dz>
2020-09-25 09:21:41 +0100

    final with notif and video calls

--- lib/chat/VideCalls/call_screen.dart
+++ lib/chat/VideCalls/call_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
 import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 import 'dart:async';
 import 'package:agora_rtc_engine/rtc_engine.dart';
@@ -29,14 +29,23 @@ class _CallScreenState extends State<CallScreen> {
   bool muted = false;
   RtcEngine _engine;
 
+  @override
+  void dispose() {
+    // clear users
+    _users.clear();
+    // destroy sdk
+    _engine.leaveChannel();
+    _engine.destroy();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     addPostFrameCallback();
     initialize();
-  }
 
+  }
   Future<void> initialize() async {
     if (APP_ID.isEmpty) {
       setState(() {
@@ -56,32 +65,16 @@ class _CallScreenState extends State<CallScreen> {
     await _engine.setVideoEncoderConfiguration(configuration);
     await _engine.joinChannel(null, widget.call.channelId, null, 0);
   }
+
+  /// Create agora sdk instance and initialize
   Future<void> _initAgoraRtcEngine() async {
     _engine = await RtcEngine.create(APP_ID);
     await _engine.enableVideo();
-    await _engine.setChannelProfile(ChannelProfile.Communication);
+    await _engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
+    await _engine.setClientRole(ClientRole.Broadcaster);
   }
 
-  addPostFrameCallback() {
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      var user = Provider.of<User>(context, listen: false);
-
-      callStreamSubscription = callMethods
-          .callStream(uid: user.uid)
-          .listen((DocumentSnapshot ds) {
-        // defining the logic
-        switch (ds.data) {
-          case null:
-          // snapshot is null which means that call is hanged and documents are deleted
-            Navigator.pop(context);
-            break;
-
-          default:
-            break;
-        }
-      });
-    });
-  }
+  /// Add agora event handlers
   void _addAgoraEventHandlers() {
     _engine.setEventHandler(RtcEngineEventHandler(error: (code) {
       setState(() {
@@ -94,7 +87,7 @@ class _CallScreenState extends State<CallScreen> {
         _infoStrings.add(info);
       });
     }, leaveChannel: (stats) {
-      callMethods.endCall(call: widget.call);
+      CallMethods().endCall();
       setState(() {
         _infoStrings.add('onLeaveChannel');
         _users.clear();
@@ -106,7 +99,7 @@ class _CallScreenState extends State<CallScreen> {
         _users.add(uid);
       });
     }, userOffline: (uid, elapsed) {
-      callMethods.endCall(call: widget.call);
+      CallMethods().endCall();
       setState(() {
         final info = 'userOffline: $uid';
         _infoStrings.add(info);
@@ -119,12 +112,15 @@ class _CallScreenState extends State<CallScreen> {
       });
     }));
   }
+
+  /// Helper function to get list of native views
   List<Widget> _getRenderViews() {
     final List<StatefulWidget> list = [];
+      list.add(RtcLocalView.SurfaceView());
+
     _users.forEach((int uid) => list.add(RtcRemoteView.SurfaceView(uid: uid)));
     return list;
   }
-
 
   /// Video view wrapper
   Widget _videoView(view) {
@@ -179,6 +175,55 @@ class _CallScreenState extends State<CallScreen> {
     return Container();
   }
 
+  /// Toolbar layout
+  Widget _toolbar() {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: const EdgeInsets.symmetric(vertical: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RawMaterialButton(
+            onPressed: _onToggleMute,
+            child: Icon(
+              muted ? Icons.mic_off : Icons.mic,
+              color: muted ? Colors.white : Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: muted ? Colors.blueAccent : Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          ),
+          RawMaterialButton(
+            onPressed: () => _onCallEnd(context),
+            child: Icon(
+              Icons.call_end,
+              color: Colors.white,
+              size: 35.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.redAccent,
+            padding: const EdgeInsets.all(15.0),
+          ),
+          RawMaterialButton(
+            onPressed: _onSwitchCamera,
+            child: Icon(
+              Icons.switch_camera,
+              color: Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          )
+        ],
+      ),
+    );
+  }
+
   /// Info panel to show logs
   Widget _panel() {
     return Container(
@@ -229,6 +274,10 @@ class _CallScreenState extends State<CallScreen> {
     );
   }
 
+  void _onCallEnd(BuildContext context) {
+    Navigator.pop(context);
+  }
+
   void _onToggleMute() {
     setState(() {
       muted = !muted;
@@ -240,67 +289,30 @@ class _CallScreenState extends State<CallScreen> {
     _engine.switchCamera();
   }
 
-  /// Toolbar layout
-  Widget _toolbar() {
-    return Container(
-      alignment: Alignment.bottomCenter,
-      padding: const EdgeInsets.symmetric(vertical: 48),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RawMaterialButton(
-            onPressed: _onToggleMute,
-            child: Icon(
-              muted ? Icons.mic : Icons.mic_off,
-              color: muted ? Colors.white : Colors.blueAccent,
-              size: 20.0,
-            ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: muted ? Colors.blueAccent : Colors.white,
-            padding: const EdgeInsets.all(12.0),
-          ),
-          RawMaterialButton(
-            onPressed: () => callMethods.endCall(
-              call: widget.call,
-            ),
-            child: Icon(
-              Icons.call_end,
-              color: Colors.white,
-              size: 35.0,
-            ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.redAccent,
-            padding: const EdgeInsets.all(15.0),
-          ),
-          RawMaterialButton(
-            onPressed: _onSwitchCamera,
-            child: Icon(
-              Icons.switch_camera,
-              color: Colors.blueAccent,
-              size: 20.0,
-            ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.white,
-            padding: const EdgeInsets.all(12.0),
-          )
-        ],
-      ),
-    );
+
+
+
+  addPostFrameCallback() {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      var user = Provider.of<User>(context);
+
+      callStreamSubscription = callMethods
+          .callStream(uid: user.uid)
+          .listen((DocumentSnapshot ds) {
+        // defining the logic
+        switch (ds.data) {
+          case null:
+          // snapshot is null which means that call is hanged and documents are deleted
+            Navigator.pop(context);
+            break;
+
+          default:
+            break;
+        }
+      });
+    });
   }
 
-  @override
-  void dispose() {
-    // clear users
-    _users.clear();
-    // destroy sdk
-    _engine.leaveChannel();
-    _engine.destroy();
-    callStreamSubscription.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -310,7 +322,7 @@ class _CallScreenState extends State<CallScreen> {
         child: Stack(
           children: <Widget>[
             _viewRows(),
-             _panel(),
+            _panel(),
             _toolbar(),
           ],
         ),
